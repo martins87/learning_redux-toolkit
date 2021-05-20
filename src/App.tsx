abbcf589eeea6f50360111f8d45fd58c7710ab40
DANIEL ALVES A MARTINS <dmartins@ciasc.local>
2021-05-20 20:55:13 -0300

    Changed some styles to Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import Button from '@material-ui/core/Button';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 // import { useDispatch, useSelector } from 'react-redux';
 
 import './App.css';
 import { increment, decrement, incrementByAmount } from './state/counterSlice';
 import { useAppDispatch, useAppSelector } from './hooks';
+
+// We can also create a separate file (e.g. theme.ts) and have every styles in there
+const useStyles = makeStyles((theme: Theme) => ({
+  count: {
+    color: 'yellow'
+  },
+  app: {
+    textAlign: 'center'
+  },
+  button: {
+    margin: '4px !important'
+  }
+}));
 
 // interface StateProps {
 //   counter: {
@@ -20,17 +34,18 @@ const App = () => {
   });
   // const dispatch = useDispatch();
   const dispatch = useAppDispatch();
+  const classes = useStyles();
 
   return (
-    <div className="App">
+    <div className={classes.app}>
       <header className="App-header">
         <p>
-          Counter: <span style={{color:"yellow"}}>{count}</span>
+          Counter: <span className={classes.count}>{count}</span>
         </p>
         <div>
-          <Button variant="contained" className="mui-btn" onClick={() => dispatch(increment())}>Increment</Button>
-          <Button variant="contained" className="mui-btn" onClick={() => dispatch(decrement())}>Decrement</Button>
-          <Button variant="contained" className="mui-btn" onClick={() => dispatch(incrementByAmount(10))}>Increment by 10</Button>
+          <Button variant="contained" className={classes.button} onClick={() => dispatch(increment())}>Increment</Button>
+          <Button variant="contained" className={classes.button} onClick={() => dispatch(decrement())}>Decrement</Button>
+          <Button variant="contained" className={classes.button} onClick={() => dispatch(incrementByAmount(10))}>Increment by 10</Button>
         </div>
       </header>
     </div>
